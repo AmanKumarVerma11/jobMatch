56924f4104af11596aaf43e4d870ed5cddb449f0
Anuj Singh Rawat <84454648+anujsinghrawat@users.noreply.github.com>
2024-05-18 11:27:09 +0530

    chore: Add proxy configuration and update ML assessment API call

--- src/components/mlAssessment.js
+++ src/components/mlAssessment.js
@@ -1,7 +1,6 @@
-// MlAssessment.js
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import Quiz from 'react-quiz-component';
 import Header from '../components/header';
 import Footer from '../components/footer';
@@ -13,22 +12,25 @@ const MlAssessment = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    console.log('useEffect called with role:', role);
     const fetchQuiz = async () => {
       try {
-        console.log('Fetching quiz data...');
-        setIsLoading(true); // Set loading state to true before fetching data
-        const response = await axios.get('http://127.0.0.1:5000/quiz', {
-          data: {
-            job_role: role,
+        setIsLoading(true);
+
+        const formData = new FormData();
+        formData.append('job_role', role);
+
+        const response = await axios.post('http://127.0.0.1:5000/quiz', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
           },
         });
-        console.log('Response:', response);
+
         setQuiz(response.data);
       } catch (error) {
         console.error('Error fetching quiz:', error);
+      } finally {
+        setIsLoading(false);
       }
-      setIsLoading(false); // Set loading state to false after fetching data
     };
 
     fetchQuiz();
@@ -41,7 +43,7 @@ const MlAssessment = () => {
         <h2 className='text-2xl'>Take your Assessment</h2>
         {isLoading ? (
           <LoadingAnimation />
-        ) : quiz && quiz.questions ? (
+        ) : quiz && quiz.question ? (
           <Quiz quiz={quiz} shuffle={true} showInstantFeedback={false} continueTillCorrect={false} />
         ) : (
           <p>No quiz data available.</p>
